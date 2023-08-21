7f96a19c1453304fd14d10b30a27debee5b319ac
michelodev95 <migueldiazdev95@gmail.com>
2023-08-21 13:25:11 +0200

    prevent input zoom

--- src/components/home/presentation/Header.tsx
+++ src/components/home/presentation/Header.tsx
@@ -40,7 +40,7 @@ const Header: React.FC<HeaderProps> = ({
           </div>
           <input
             type="text"
-            className="block p-2 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg w-full bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            className="block p-2 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg w-full bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 no-zoom"
             placeholder="Search for country"
             onChange={(e) => setFilterCountry(e.target.value)}
           />
